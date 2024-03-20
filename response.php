52847b91413e5332e30d616310afafc4a5215e87
vickypandey14 <vickypandeyts@gmail.com>
2024-03-20 16:24:50 +0530

    comments added

--- response.php
+++ response.php
@@ -9,6 +9,9 @@ $data = json_decode(file_get_contents("php://input"));
 
 $text = $data->text;
 
+// Enter Your Gemini API Key Here....
+// Replace YOUR_GEMINI_API_KEY_HERE with your API key
+
 $client = new Client("YOUR_GEMINI_API_KEY_HERE");
 
 $response = $client->geminiPro()->generateContent(
