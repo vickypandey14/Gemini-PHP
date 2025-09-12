077683e2bbd55a965d8bdb01a8fd6f88b29feed7
Vivek Pandey <vickypandeyts@gmail.com>
2025-09-13 00:05:13 +0530

    refactor: move inline JS from index.php to script.js and create custom CSS file; linked both style.css and script.js in index.php

--- public/js/script.js
+++ public/js/script.js
@@ -5,44 +5,51 @@ function generateResponse() {
     var spinner = button.querySelector(".spinner-border");
 
     if (text.trim() === "") {
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Please enter some text before generating response!',
-        });
-        return;
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Please enter some text before generating response!',
+    });
+    return;
     }
 
     button.disabled = true;
     spinner.classList.remove("d-none");
+    response.innerHTML = ""; // clear previous
 
     fetch("response.php", {
-        method: "post",
-        body: JSON.stringify({
-            text: text
-        }),
+    method: "post",
+    body: JSON.stringify({ text: text }),
     })
-        .then(res => res.text())
-        .then(res => {
-            spinner.classList.add("d-none");
-            button.disabled = false;
-            typeResponse(res, response);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            spinner.classList.add("d-none");
-            button.disabled = false;
-        });
+    .then(res => res.text())
+    .then(res => {
+        spinner.classList.add("d-none");
+        button.disabled = false;
+        typeResponse(res, response);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        spinner.classList.add("d-none");
+        button.disabled = false;
+    });
 }
 
 function typeResponse(responseText, element) {
-    var index = 0;
-    var typingEffect = setInterval(function() {
+    let index = 0;
+    element.innerHTML = "";
+    
+    const cursor = document.createElement("span");
+    cursor.classList.add("cursor");
+    element.appendChild(cursor);
+
+    let typingEffect = setInterval(function () {
         if (index < responseText.length) {
-            element.innerHTML += responseText.charAt(index);
-            index++;
+            let chunk = responseText.substr(index, 5);
+            cursor.insertAdjacentHTML("beforebegin", chunk);
+            index += 5;
         } else {
             clearInterval(typingEffect);
+            cursor.remove();
         }
-    }, 10);
+    }, 5);
 }
